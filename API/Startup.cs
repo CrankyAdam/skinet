faa52bae763e3a4613ed421c60106b72f49c90b4
Adam M <amathison@crankyape.com>
2020-07-14 09:33:52 -0500

    Added Swagger and orginized/cleaned up start up file

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -24,20 +24,42 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>))); 
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext>(x =>
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            // services.AddDbContext<AppIdentityDbContext>(x => 
+            // {
+            //     x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            // });
+
+            // services.AddSingleton<IConnectionMultiplexer>(c => {
+            //     var configuration = ConfigurationOptions.Parse(_config
+            //         .GetConnectionString("Redis"), true);
+            //     return ConnectionMultiplexer.Connect(configuration);
+            // });
+
+            services.AddApplicationServices();
+            // services.AddIdentityServices(_config);
+            services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+    
+            app.UseMiddleware<ExceptionMiddleware>();
+            
+            app.UseStatusCodePagesWithReExecute("/errors/{0}"); 
 
             app.UseHttpsRedirection();
 
@@ -46,6 +68,8 @@ namespace API
             app.UseStaticFiles(); 
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumention(); 
 
             app.UseEndpoints(endpoints =>
             {
