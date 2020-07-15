3a0024e21c41bcb8784420a6983e09493fe0b501
Adam M <amathison@crankyape.com>
2020-07-15 16:14:53 -0500

    added components, filtering, sorting and searching func thru Angular

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/products';
+
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+ @Input() product: IProduct;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
